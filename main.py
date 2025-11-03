f8690d2132b6407a41abc86c6f7827fcac1abbb9
hunt82346141-afk <hunt82346141@gmail.com>
2025-11-03 13:17:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st  
 st.title("나의 웹 서비스 만들기!!")
 name=st.text_input('이름을 알려주세요:')
-st.selectbox('좋아하는 음식을 선택해주세요:',['스시','오꼬노마야끼'])
+menu=st.selectbox('좋아하는 음식을 선택해주세요:',['스시','오꼬노마야끼'])
 if st.button('인사말 생성'):
   st.info(name+'님 안녕하세요')
-  st.warning('반가워요')
+  st.warning(menu+'좋아하시나봐요')
   st.error('정말 반가워요')
   st.balloons()
