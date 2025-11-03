a76b3608ed91072ac76a80b88fd53100820a7adf
hunt82346141-afk <hunt82346141@gmail.com>
2025-11-03 13:20:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ name=st.text_input('이름을 알려주세요:')
 menu=st.selectbox('좋아하는 음식을 선택해주세요:',['스시','오꼬노마야끼'])
 if st.button('인사말 생성'):
   st.info(name+'님 안녕하세요')
-  st.warning(menu+'를 좋아하시나봐요 저는 싫어하는데')
+  st.warning(menu+'를 좋아하시나봐요 저는 싫어하는데ㅋ')
   st.error('정말 반가워요')
   st.balloons()
